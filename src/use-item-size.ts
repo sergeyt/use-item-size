10c03dcecc2e8fffa194635b77c385793e975961
Sergey Todyshev <stodyshev@gmail.com>
2020-11-07 16:38:50 +0700

    extract useMeasure

---
+++ src/use-item-size.ts
@@ -0,0 +1,55 @@
+import { useCallback } from "react";
+
+import { useCache } from "./use-cache";
+import { useMeasure } from "./use-measure";
+
+function defaultGetKey(rec: any) {
+  return rec._id;
+}
+
+type RenderFn = ({
+  index,
+  measuring,
+}: Partial<{ index: number; measuring: boolean }>) => any;
+
+type Options = {
+  id: string;
+  data: any[];
+  children: RenderFn;
+  width: number;
+  getKey?: (rec: any) => any;
+  refresh: () => void;
+  cacheDeps?: any;
+};
+
+export function useItemSize({
+  id,
+  data,
+  children,
+  width,
+  getKey = defaultGetKey,
+  refresh,
+  cacheDeps,
+}: Options) {
+  const cache = useCache({ data, width, ...cacheDeps }, refresh);
+  const measure = useMeasure({ id, width });
+
+  const itemSize = useCallback(
+    (index) => {
+      const rec = data[index];
+      const key = getKey(rec);
+      let result: number;
+      if (!cache.has(key)) {
+        const height = measure(children({ index, measuring: true }));
+        cache.set(key, height);
+        result = height;
+      } else {
+        result = cache.get(key) as number;
+      }
+      return result;
+    },
+    [data, getKey, cache, children, measure]
+  );
+
+  return itemSize;
+}
---
+++ src/use-measure.ts
@@ -0,0 +1,67 @@
+import { useCallback, useEffect, useRef } from "react";
+import { renderToString } from "react-dom/server";
+import forEach from "lodash/forEach";
+import isNumber from "lodash/isNumber";
+
+const containerStyle = {
+  display: "inline-flex",
+  position: "absolute",
+  visibility: "hidden",
+  zIndex: -1,
+};
+
+function createLayer(id: string) {
+  const container = document.createElement("div");
+  container.setAttribute("id", id);
+  forEach(containerStyle, (v, k) => {
+    container.style[k] = v;
+  });
+  return container;
+}
+
+export function measureElement(
+  layerId: string,
+  content: React.ReactElement,
+  width?: number
+) {
+  const container = document.getElementById(layerId) || createLayer(layerId);
+
+  if (isNumber(width)) {
+    container.style.width = width + "px";
+  }
+  container.innerHTML = renderToString(content);
+  document.body.appendChild(container);
+
+  const height = container.clientHeight;
+
+  return { height, container };
+}
+
+type Options = {
+  id?: string;
+  width?: number;
+};
+
+export function useMeasure({ id, width }: Options = {}) {
+  const layerId = `${id}-measure-layer`;
+  const containerRef = useRef<any>(null);
+
+  const measure = useCallback(
+    (content: React.ReactElement) => {
+      const { height, container } = measureElement(layerId, content, width);
+      containerRef.current = container;
+      return height;
+    },
+    [layerId, width]
+  );
+
+  useEffect(() => {
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.parentNode?.removeChild(containerRef.current);
+      }
+    };
+  }, [layerId]);
+
+  return measure;
+}
